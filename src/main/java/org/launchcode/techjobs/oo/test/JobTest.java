8ef99ec4d93f6184672a8e9fb0456e29e57bcdb5
Masterlynx2 <Tonywatkins325@gmail.com>
2022-02-07 20:03:38 -0500

    changing my testSettingJobID

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -20,41 +20,7 @@ import static org.junit.Assert.*;
 
 public class JobTest {
 
-//@FixMethodOrder(MethodSorters.DEFAULT)
-//public class JUnit4TestOrder{
-//    @Test
-//        public void testSettingJobId(){
-//        System.out.println("testSettingJobId executes");
-//    }
-//    @Test
-//    public void testJobConstructorSetsAllFields(){
-//        System.out.println("testJobConstructorSetsAllFields executes");
-//    }
-//    @Test
-//    public void testJobsForEquality(){
-//        System.out.println("testJobsForEquality executes");
-//    }
-//
 
-
-
-//        public void
-       //@Before
-//        Job a1= new Job("performer","launchcode","detroit","singer","vocals");
-
-//    Job job1;
-//    Job job2;
-//    Job tester5;
-//    Job tester8;
-   // @Before
-   // public void Job() {
-//        job1 = new Job();
-//        job2 = new Job();
-//        tester = new Job("", new Employer("ACME"), new Location(""), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-//        tester2 = new Job("", new Employer("ACME"), new Location(""), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        //job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-         //job = new Job("Web Developer", new Employer("LaunchCode"), new Location("StL"), new PositionType("Back-end developer"), new CoreCompetency("Java"));
-   // }
 
     @Test
     public void testSettingJobId() {
@@ -103,18 +69,7 @@ public class JobTest {
         assertEquals(firstChar, '\n');
         assertEquals(lastChar, '\n');
     }
-//    @Test
-//    public void testToStringLabel() {
-//
-//        Boolean labelId = tester.toString().contains("ID");
-//        Boolean labelName = tester.toString().contains("Name");
-//        Boolean labelEmployer = tester.toString().contains("Employer");
-//        Boolean labelLocation = tester.toString().contains("Location");
-//        Boolean labelPositionType = tester.toString().contains("PositionType");
-//        Boolean labelCoreCompetency = tester.toString().contains("CoreCompetency");
-//
-//        assertTrue(labelId && labelName && labelEmployer && labelLocation && labelPositionType && labelCoreCompetency == true);
-    //}
+
     @Test
     public void testToStringContainsCorrectLabelsAndData() {
         Job job = new Job("", new Employer("ACME"), new Location(""), new PositionType("Quality control"), new CoreCompetency("Persistence"));
@@ -127,7 +82,7 @@ public class JobTest {
         Boolean labelCoreCompetency = job.toString().contains("Core Competency");
 
         assertTrue(labelId && labelName && labelEmployer && labelLocation && labelPositionType && labelCoreCompetency == true);
-//st
+
 
         String test = "\nID: " + job.getId() + "\n" +
                 "Name: Data not available\n" +
@@ -136,12 +91,12 @@ public class JobTest {
                 "Position Type: Quality control\n" +
                 "Core Competency: Persistence\n";
         assertEquals(test, job.toString());
-//        assertEquals(tester.toString(), tester.toString());
+
     }
     @Test
     public void testToStringHandlesEmptyField(){
         Job job = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
-        //System.out.println(tester.getLocation().getValue());
+
         assertEquals("OOPS! This job does not seem to exist.",job.toString());
     }
 
